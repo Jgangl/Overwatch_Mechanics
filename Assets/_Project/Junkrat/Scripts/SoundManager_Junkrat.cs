bdd533199acf47bc6976c2fb91f8f2268dc738f8
Jgangl <joshua.gangl@gmail.com>
2022-02-18 21:48:17 -0500

    Moving Sound scripts

--- Assets/_Project/Junkrat/Scripts/SoundManager_Junkrat.cs
+++ Assets/_Project/Junkrat/Scripts/SoundManager_Junkrat.cs
@@ -7,8 +7,6 @@ public class SoundManager_Junkrat : MonoBehaviour
     [SerializeField] private AudioClip _beepClip;
     [SerializeField] private float _beepTime;
     [SerializeField] private AudioClip _grenadeShootClip;
-    [SerializeField] private AudioClip _grenadeExplodeClip;
-    [SerializeField] private AudioClip _mineExplodeClip;
 
     private static SoundManager_Junkrat _instance;
 
@@ -18,9 +16,7 @@ public class SoundManager_Junkrat : MonoBehaviour
     }
 
     [SerializeField] AudioSource _beepSource;
-    [SerializeField] AudioSource _grenadeShootSource;
-    [SerializeField] AudioSource _grenadeExplodeSource;
-    [SerializeField] AudioSource _mineExplodeSource;
+    [SerializeField] private AudioSource _grenadeShootSource;
 
     private void Awake()
     {
@@ -32,50 +28,9 @@ public class SoundManager_Junkrat : MonoBehaviour
         }
     }
 
-    private void Update()
-    {
-
-    }
-
-    IEnumerator BeepTwiceCoroutine()
-    {
-        PlayBeepOnce();
-
-        //float beepClipLength = _beepClip.length;
-
-        yield return new WaitForSeconds(_beepTime);
-        
-        PlayBeepOnce();
-    }
-
-    public void PlayBeepOnce()
-    {
-        _beepSource.Stop();
-
-        _beepSource.clip = _beepClip;
-        _beepSource.Play();
-    }
-
-    public void PlayBeepTwice()
-    {
-        StartCoroutine(BeepTwiceCoroutine());
-    }
-    
     public void PlayGrenadeShoot()
     {
         _grenadeShootSource.clip = _grenadeShootClip;
         _grenadeShootSource.Play();
-    }
-    
-    public void PlayGrenadeExplode()
-    {
-        _grenadeExplodeSource.clip = _grenadeExplodeClip;
-        _grenadeExplodeSource.Play();
-    }
-
-    public void PlayMineExplode()
-    {
-        _mineExplodeSource.clip = _mineExplodeClip;
-        _mineExplodeSource.Play();
     }
 }
